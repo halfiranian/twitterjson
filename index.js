14140e1b772b7e98eaedb27b812cd11f7c480fa4
Halfiranian <jamessadri@gmail.com>
2014-01-30 23:23:37 +0000

    fixed heroku?

--- index.js
+++ index.js
@@ -18,9 +18,6 @@ app.get("/tweets.json", function(req, res) {
     T.get('search/tweets', { q: req.query.search, count: 100, result_type: 'popular' }, function(err, reply) {
     // this is important -
     // you must use Response.json()
-    if(err){
-        res.send("this really isn't working");
-    }
     res.jsonp(reply);
     console.log(req.query.search);
     })
